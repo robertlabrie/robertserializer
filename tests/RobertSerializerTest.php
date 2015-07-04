5f186ceafb973b5e5e79b437dfef9ed48a152928
Robert Labrie <robert.labrie@gmail.com>
2015-07-04 17:18:26 -0400

    flesh out some tests

--- tests/RobertSerializerTest.php
+++ tests/RobertSerializerTest.php
@@ -3,5 +3,60 @@ require_once __DIR__."/../vendor/autoload.php";
 require_once __DIR__."/TestClasses.php";
 class RobertSerializerTest extends PHPUnit_Framework_TestCase
 {
-
+	public $rs;
+	public function setUp()
+	{
+		$this->rs = new \RobertSerializer\RobertSerializer();
+	}
+	public function testHashArraySimple()
+	{
+		$in = Array('a'=>1,'b'=>2);
+		$out = $this->rs->deserialize($this->rs->serialize($in));
+		$this->assertEquals($in,$out);
+	}
+	public function testObjectSimple()
+	{
+		$in = new \RobertSerializerTestClasses\DummyB();
+		$ser = $this->rs->serialize($in);
+		$expected = Array('__TYPE__' => 'RobertSerializerTestClasses\dummyB','p'=> "I'm a public property.",'i'=>"I'm a private property.");
+		$this->assertEquals($ser,$expected);
+		
+		$out = $this->rs->deserialize($ser);
+		$this->assertEquals($in,$out);
+	}
+	public function testObjectBusy()
+	{
+		$in = new \RobertSerializerTestClasses\DummyA();
+		$in->makeBusy();
+		
+		$ser = $this->rs->serialize($in);
+		$expected = Array(
+			'__TYPE__'=>'RobertSerializerTestClasses\dummyA',
+			'a'=>Array(
+				'a'=>1,
+				'b'=>2
+			),
+			'n'=>Array(1,2,3),
+			's'=>'I am a String',
+			'i'=>1,
+			'b'=>true,
+			'o'=>Array(
+				'__TYPE__'=>'RobertSerializerTestClasses\dummyB',
+				'p' => "I'm a public property.",
+				'i' => "I'm a private property.",
+			),
+		);
+		$this->assertEquals($ser,$expected);
+		
+		$out = $this->rs->deserialize($ser);
+		$this->assertEquals($in,$out);
+	}
+	public function testBusyArray()
+	{
+		$in = Array(new \RobertSerializerTestClasses\DummyB(),new \RobertSerializerTestClasses\DummyB());
+		$busy = new \RobertSerializerTestClasses\DummyA();
+		$busy->makeBusy();
+		array_push($in,$busy);
+		$this->assertEquals($in,$this->rs->deserialize($this->rs->serialize($in)));
+	}
 }
--- tests/TestClasses.php
+++ tests/TestClasses.php
@@ -1,17 +1,28 @@
 <?php
 
-namespace TestClasses;
+namespace RobertSerializerTestClasses;
 
 class dummyA
 {
-	public $a = Array('a'=>1,'b'=>2);
-	public $n = Array(1,2,3);
-	public $s = "I am a String";
-	public $i = 1;
-	private $b = true;
+	public $a;
+	public $n;
+	public $s;
+	public $i;
+	private $b;
 	public $o;
-	public function __construct()
+	public function makeBusy()
 	{
-		$this->o = new \RobertSerializer();
+		$this->a = Array('a'=>1,'b'=>2);
+		$this->n = Array(1,2,3);
+		$this->s = "I am a String";
+		$this->i = 1;
+		$this->b = true;
+		$this->o = new \RobertSerializerTestClasses\dummyB();
 	}
+}
+
+class dummyB
+{
+	public $p = "I'm a public property.";
+	private $i = "I'm a private property.";
 }
